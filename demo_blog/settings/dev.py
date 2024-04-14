21f339c4369c5091392bd1045b007da94196392c
Andrei <betoxd2003@gmail.com>
2024-04-13 22:55:16 -0400

    Inicializando proyecto

---
+++ demo_blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+p+0maoz9+r1iw=_s5iw#&eg-y%r*8)hf!azoj)gvg^*f#p4ir"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
